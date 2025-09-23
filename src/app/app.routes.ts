90b2a8dccdec68bf1ee5ab839135210fbecdbeeb
Enriquecarvajalnunez <63138636+Enriquecarvajalnunez@users.noreply.github.com>
2025-09-22 21:10:11 -0500

    ultimos cambios

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,29 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './servicios/features/home/home.component';
+import { LoginComponent } from './servicios/features/login/login.component';
+import { AdminComponent } from './servicios/features/admin/admin.component';
 
 export const routes: Routes = [
+    { path: 'home', 
+        component: HomeComponent 
+
+    },
+    { path: 'login', 
+        component: LoginComponent 
+    },
+    {
+        path: 'admin',
+        component: AdminComponent
+    },
     {
         path: 'servicios',
         loadChildren: () =>
             import('./servicios/features/servicios-shell/servicios.route'),
     },
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
     {
         path: '**',
-        redirectTo: 'servicios',
+        redirectTo: 'home',
     }
+    
 ];
---
+++ src/app/servicios/features/admin/admin.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>admin</p>
+  `,
+  styles: ``
+})
+export class AdminComponent {
+
+}
